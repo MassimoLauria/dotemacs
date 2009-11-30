bdd0fcf134b933a5dbe196e8354d1785f10b365e
Massimo Lauria <lauria.massimo@gmail.com>
2009-11-30 20:38:02 +0100

    Journal support in org-mode

--- init-org-mode.el
+++ init-org-mode.el
@@ -6,14 +6,15 @@
 
 (setq org-directory "~/personal/organizer/")
 (setq org-default-notes-file (concat org-directory "notes.org"))
-(setq org-agenda-files (list "~/personal/organizer/"))
-
+(setq org-default-journal-file (concat org-directory "journal.org"))
+(setq org-agenda-files (list org-directory))
 
 ;;;-------------------------------------------------------------------
 
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cr" 'org-remember)
+(define-key global-map "\C-cj" 'org-add-journal-entry)
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
@@ -27,6 +28,16 @@
           	    (org-set-local 'yas/trigger-key [tab])
           	    (define-key yas/keymap [tab] 'yas/next-field-group)))
 
+(defun org-add-journal-entry ()
+  "Start a new journal entry."
+  (interactive)
+  (find-file org-default-journal-file)
+  (goto-char (point-min))
+  (org-insert-heading)
+  (org-insert-time-stamp (current-time) t)
+  (open-line 2)
+  (insert " "))
+ 
 
 ;; Local Variables:
 ;; mode: emacs-lisp 
