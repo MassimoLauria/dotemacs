80a85d50dd955dd55aceeff9d8c584742b463ddb
Massimo Lauria <lauria.massimo@gmail.com>
2010-03-10 15:14:38 +0100

    TODO: fix upgrade of autocomplete

--- init.el
+++ init.el
@@ -352,3 +352,14 @@ Otherwise, analyses point position and answers."
 ;; folded-file: t
 ;; End: 
 
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
