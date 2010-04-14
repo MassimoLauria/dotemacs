f7037c7a239380281d6f006672d255c2e463afc2
Massimo Lauria <lauria.massimo@gmail.com>
2010-04-14 18:24:16 +0200

    Customization for Ebib

--- custom.el
+++ custom.el
@@ -6,6 +6,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ac-sources (quote (ac-source-yasnippet ac-source-imenu ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-filename)) t)
+ '(ebib-index-display-fields (quote (year author title)))
+ '(ebib-print-multiline t)
+ '(ebib-sort-order (quote ((year) (author editor) (title))))
  '(safe-local-variable-values (quote ((ispell-default-dictionary . american) (Tex-PDF-mode . t) (folded-file . t))))
  '(show-paren-mode t))
 (custom-set-faces
