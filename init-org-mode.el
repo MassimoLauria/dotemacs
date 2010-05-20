f21cc852bc4954eb38cc5a08a9f4e37d824c6d3a
Massimo Lauria <lauria.massimo@gmail.com>
2010-05-20 18:09:31 +0200

    A sensible and useful org-remember configuration.

--- init-org-mode.el
+++ init-org-mode.el
@@ -14,7 +14,6 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cr" 'org-remember)
-(define-key global-map "\C-cj" 'org-add-journal-entry)
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
@@ -97,19 +96,11 @@
 
 (org-remember-insinuate)
 (setq org-remember-templates
-      '(("Journal" ?j "* %? %U\n\n  %i\n  %a" "journal.org" "Diario")
-        ("Idea" ?i "* %^{Title}\n  %i\n  %a" "notes.org" "New Ideas")))
-
-
-(defun org-add-journal-entry ()
-  "Start a new journal entry."
-  (interactive)
-  (find-file org-default-journal-file)
-  (goto-char (point-min))
-  (org-insert-heading)
-  (org-insert-time-stamp (current-time) t)
-  (open-line 2)
-  (insert " "))
+      '(
+        ("Journal" ?j "* %? %U\n\n  %i\n  %a" "journal.org")
+        ("Leggere" ?l "* Leggere “%:title” %U\n\n  “%:title”, by\n  %:author (%:year)\n\n  %?\n\n  BibItem-> %a" "notes.org")
+        ("Idea"    ?i "* %? %U\n\n  %i\n  %a" "notes.org")
+      )
 
 
 ;; Local Variables:
