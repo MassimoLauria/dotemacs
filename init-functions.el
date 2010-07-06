9d015303047ffc74dbb26215b84d739f9f17a134
Massimo Lauria <lauria.massimo@gmail.com>
2010-07-06 12:22:01 +0200

    A new function which kills compilation window after success

--- init-functions.el
+++ init-functions.el
@@ -202,6 +202,23 @@ either ispell or flyspell, if the latter is active"
   nil)
 
 
+;;----- Fix compile modes ------------------------------------------------
+
+;; Helper for compilation. Close the compilation window if
+;; there was no error at all.
+(defun compilation-exit-autoclose (status code msg)
+  ;; If M-x compile exists with a 0
+  (when (and (eq status 'exit) (zerop code))
+    ;; then bury the *compilation* buffer, so that C-x b doesn't go there
+    (bury-buffer "*compilation*")
+    ;; and delete the *compilation* window
+    (delete-window (get-buffer-window (get-buffer "*compilation*"))))
+  ;; Always return the anticipated result of compilation-exit-message-function
+  (cons msg code))
+;; Specify my function (maybe I should have done a lambda function)
+(setq compilation-exit-message-function 'compilation-exit-autoclose)
+
+
 
 ;;----- Additional modes -—-------------------------------------------------
 (define-generic-mode vimrc-mode
