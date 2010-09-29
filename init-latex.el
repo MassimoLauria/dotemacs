3442ed049786c0a3865379dc0e7f337d90396759
Massimo Lauria <lauria.massimo@gmail.com>
2010-09-29 16:39:51 +0200

    RefTeX autolabelling ready for math

--- init-latex.el
+++ init-latex.el
@@ -33,6 +33,7 @@
     (TeX-fold-buffer)
     )
   )
+
 (add-hook 'LaTeX-mode-hook 
           (lambda () (progn 
                        ;;(TeX-fold-mode 1) 
@@ -45,6 +46,8 @@
                        )
             ))
   
+
+
 
 ;; Math writing facilities
 ;; (setq TeX-electric-escape t)
@@ -76,7 +79,30 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-  
+
+;; RefTeX hint for automatic ref creations
+(setq reftex-label-alist 
+      '(
+        ("definition" ?d "def:"  "~\\ref{%s}" nil ("definition" "def.") -3) 
+
+        ("theorem"    ?h "thm:"  "~\\ref{%s}" t   ("th." "theorem") -3)
+        ("lemma"      ?l "lmm:"  "~\\ref{%s}" t   ("lemma") -3) 
+        ("corollary"  ?c "cor:"  "~\\ref{%s}" nil ("corollary"  "cor.") -3) 
+
+        ("fact"        ?F "fact:"  "~\\ref{%s}" nil ("fact") -3)
+        ("claim"       ?C "clm:"   "~\\ref{%s}" nil ("claim") -3)
+        ("proposition" ?S "stm:"   "~\\ref{%s}" nil ("proposition" "prop.") -3)
+
+        ("remark"      ?S "stm:"  "~\\ref{%s}" nil ("remark") -3)
+        ("property"    ?S "stm:"  "~\\ref{%s}" nil ("property") -3)
+
+        ("example"    ?g "eg:"   "~\\ref{%s}" nil ("example"  "ex." "e.g.") -3)
+        ("exercise"   ?x "ex:"   "~\\ref{%s}" nil ("exercise") -3)
+
+        ("open.problem" ?o "open:"   "~\\ref{%s}" nil ("problem") -3)
+        ("problem"      ?p "prob:"   "~\\ref{%s}" nil ("problem") -3)
+       
+        ))
  
 ;; TeX asks for Flyspell and American dictionary.
 (add-hook 'LaTeX-mode-hook (lambda () (flyspell-mode 1)))
@@ -99,6 +125,26 @@
 (add-hook 'post-command-hook 'my-flymake-show-help)
 
 
+;; Drag and Drop on Mac
+(when-running-Aquamacs
+ (add-hook
+  'LaTeX-mode-hook
+  (lambda ()
+    (smart-dnd-setup
+     '(
+       ("\\.tex\\'" . "\\input{%r}\n")
+       ("\\.cls\\'" . "\\documentclass{%f}\n")
+       ("\\.sty\\'" . "\\usepackage{%f}\n")
+       ("\\.eps\\'" . "\\includegraphics[]{%r}\n")
+       ("\\.ps\\'"  . "\\includegraphics[]{%r}\n")
+       ("\\.pdf\\'" . "\\includegraphics[]{%r}\n")
+       ("\\.jpg\\'" . "\\includegraphics[]{%r}\n")
+       ("\\.png\\'" . "\\includegraphics[]{%n}\n")
+       ("\\.mov\\'" . 
+        "\\includemovie[\n\tposter,\n\trepeat=1,\n\ttext=(%r)\n\t]{}{}{%r}\n")
+       ("\\.avi\\'" . 
+        "\\includemovie[\n\tposter,\n\trepeat=1,\n\ttext=(%r)\n\t]{}{}{%r}\n")))))
+ )
 
 ;; Local Variables:
 ;; mode: emacs-lisp 
