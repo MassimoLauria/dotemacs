6ce5d720d016ce93e579ed2ace082799925fadc6
Massimo Lauria <lauria.massimo@gmail.com>
2010-10-04 21:03:27 +0200

    A single init-preference file for Mac and X11

--- init-preferences.el
+++ init-preferences.el
@@ -3,6 +3,21 @@
 ;;;
 ;;;-----------------------------------------------------------------
 
+;; Regular fonts
+(setq 
+ font-X11-no-antialias "-misc-fixed-medium-r-normal--14-120-100-100-c-90-iso10646-1"
+ font-X11-antialias    "Monospace-10"
+ font-Mac-antialias    "Monaco-12"
+ )
+
+;; Bigger fonts
+(when (and (boundp 'prefs-activate-bigfont) prefs-activate-bigfont) 
+  (setq 
+   font-X11-no-antialias "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1"
+   font-X11-antialias    "Monospace-14"
+   font-Mac-antialias    "Monaco-14"
+   ))
+
 
 ;;; Visual preferences for Emacs 23 and above 
 
@@ -10,17 +25,21 @@
 ;; BUG: color-theme-zenburn must be loaded AFTER setting the fonts.
 
 (unless running-GNUEmacs22  ;; Default font for Emacs >22.
-  ;; Default fonts
-  (set-default-font "Monospace-10")
-  (add-to-list 'default-frame-alist '(font . "Monospace-10"))
-  ;; Fonts for small screens
-  (when (and (boundp 'prefs-activate-bigfont) prefs-activate-bigfont)
-    (set-default-font "Monospace-14")
-    (add-to-list 'default-frame-alist '(font . "Monospace-14"))
-    )
+  ;; Aquamacs main font
+  (when-running-Aquamacs
+   (set-default-font font-Mac-antialias)
+   (add-to-list 'default-frame-alist `(font . ,font-Mac-antialias))
+   )
+  ;; X11 main font
+  (unless running-Aquamacs
+   (set-default-font font-X11-antialias)
+   (add-to-list 'default-frame-alist `(font . ,font-X11-antialias))
+   )
   ;; Color theme (not available on default Emacs22 for MacOSX)
-  (require-maybe 'zenburn)
-  (when-available 'color-theme-zenburn (color-theme-zenburn))
+  (and 
+   (require-maybe 'color-theme)
+   (require-maybe 'zenburn)
+   (when-available 'color-theme-zenburn (color-theme-zenburn)))
   )
 
 ;;; Visual preferences for Emacs 22 
@@ -29,14 +48,17 @@
 ;; BUG: color-theme-zenburn must be loaded BEFORE setting the fonts.
 
 ;; Font for system with no anti-alias support (e.g. Emacs 22 on X11).
-(setq no-anti-alias-font "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1")
 (when running-GNUEmacs22
   ;; Color theme (not available on default Emacs22 for MacOSX)
-  (require-maybe 'zenburn)
-  (when-available 'color-theme-zenburn (color-theme-zenburn))
+  (and 
+   (require-maybe 'color-theme)
+   (require-maybe 'zenburn)
+   (when-available 'color-theme-zenburn (color-theme-zenburn)))
   ;; Default fonts
-  (when running-X11-process (set-default-font no-anti-alias-font))
+  (when running-X11-process (set-default-font font-X11-no-antialias))
   )
+
+
 
 ;; Calendar localization
 (setq calendar-week-start-day 1
@@ -46,10 +68,11 @@
                                  "Giugno" "Luglio" "Agosto" "Settembre" 
                                  "Ottobre" "Novembre" "Dicembre"])
 
-
 (when-available 'scroll-bar-mode (scroll-bar-mode -1)) ;; scroll-bar-mode undefined in terminal emacs!
 (when-available 'tool-bar-mode   (tool-bar-mode -1)  ) ;;   tool-bar-mode undefined in terminal emacs!
 (menu-bar-mode -1)
+(when-available 'tabbar-mode     (tabbar-mode -1)    ) ;;     tabbar-mode defined only in Aquamacs
+
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
