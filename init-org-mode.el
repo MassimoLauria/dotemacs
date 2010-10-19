eef3f5b6cf29317ae0f53d021f80424acf5b6673
Massimo Lauria <lauria.massimo@gmail.com>
2010-10-19 16:01:10 +0200

    Control-Shift keys in org-mode

--- init-org-mode.el
+++ init-org-mode.el
@@ -1,7 +1,7 @@
 ;;;
 ;;; Utilities for robust and safe emacs usage.
-;;; 
-;;; In Emacs 22 or in some terminals the keybindings may not work properly. 
+;;;
+;;; In Emacs 22 or in some terminals the keybindings may not work properly.
 ;;;
 ;;;------------------------------------------------------------------
 
@@ -25,7 +25,7 @@
 (when (> (string-to-number org-version) 5) ;; Newer versions of Org-mode
   (setq org-todo-keywords '((sequence "TODO" "FEEDBACK" "WAIT" "|" "DONE" "CANCELED" "DELEGATED")))
   )
-  
+
 
 ;;;-------------------------------------------------------------------
 
@@ -35,10 +35,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
-(setq 
+(setq
  org-log-done t
- ;; org-CUA-compatible t
- ;; org-support-shift-select t
+ org-CUA-compatible t
+ org-support-shift-select t
  org-cycle-emulate-tab nil
  org-cycle-global-at-bob t
  org-highlight-latex-fragments-and-specials t
@@ -48,20 +48,20 @@
 ;; ;; Org-mode Keys
 (setq org-replace-disputed-keys t)
 (setq org-disputed-keys '(
-                         ([(tab)]      . [(meta tab)]) 
-                         ([(shift up)]      . [(control è)]) 
-                         ([(shift down)]    . [(control à)]) 
+                         ([(tab)]      . [(meta tab)])
+                         ([(shift up)]      . [(control è)])
+                         ([(shift down)]    . [(control à)])
                          ([(shift left)]    . [(control ò) ])
-                         ([(shift right)]   . [(control ù) ]) 
-                         ([(meta  up)]      . [(meta è) ]) 
-                         ([(meta  down)]    . [(meta à) ]) 
+                         ([(shift right)]   . [(control ù) ])
+                         ([(meta  up)]      . [(meta è) ])
+                         ([(meta  down)]    . [(meta à) ])
                          ([(meta  left)]    . [(meta ò) ])
                          ([(meta  right)]   . [(meta ù) ])
-                         ([(meta  shift up)]      . [(control meta è) ]) 
-                         ([(meta  shift down)]    . [(control meta à) ]) 
+                         ([(meta  shift up)]      . [(control meta è) ])
+                         ([(meta  shift down)]    . [(control meta à) ])
                          ([(meta  shift left)]    . [(control meta ò) ])
                          ([(meta  shift right)]   . [(control meta ù) ])
-                         ([(control shift right)] . [(meta shift +)]) 
+                         ([(control shift right)] . [(meta shift +)])
                          ([(control shift left)] . [(meta shift -)])
                          ))
 
@@ -72,42 +72,52 @@
   (local-unset-key "\t")                     ; Use Tab for more decent things (auto-complete or yasnippet)
   (local-unset-key [tab])
   (local-unset-key [(tab)])
- 
+
   (local-set-key (kbd "M-<tab>")    'org-cycle)  ; Use M-Tab for
                                                  ; org-cycle, which
                                                  ; avoid to use the
                                                  ; overloaded TAB key
-  
+
   (local-set-key (kbd "M-SPC")      'org-cycle)  ; Use M-Space for
                                                  ; org-cycle, which is
                                                  ; similar to what I
                                                  ; use for
                                                  ; folding-toggle-show-hide
- 
-  
+
+
   ;; Seems to work in X window
-  (local-set-key (kbd "C-è") 'org-shiftup    ) 
-  (local-set-key (kbd "C-à") 'org-shiftdown  ) 
-  (local-set-key (kbd "C-ò") 'org-shiftleft  ) 
-  (local-set-key (kbd "C-ù") 'org-shiftright ) 
-  
+  (local-set-key (kbd "C-è") 'org-shiftup    )
+  (local-set-key (kbd "C-à") 'org-shiftdown  )
+  (local-set-key (kbd "C-ò") 'org-shiftleft  )
+  (local-set-key (kbd "C-ù") 'org-shiftright )
+
   ;; Seems to work in my Xterm
   (local-set-key "\e[27;5;232~" 'org-shiftup)
   (local-set-key "\e[27;5;224~" 'org-shiftdown)
   (local-set-key "\e[27;5;242~" 'org-shiftleft)
   (local-set-key "\e[27;5;249~" 'org-shiftright)
- 
-  (local-set-key (kbd "M-è") 'org-metaup    ) 
-  (local-set-key (kbd "M-à") 'org-metadown  ) 
-  (local-set-key (kbd "M-ò") 'org-metaleft  ) 
-  (local-set-key (kbd "M-ù") 'org-metaright ) 
- 
+
+  (local-set-key (kbd "M-è") 'org-metaup    )
+  (local-set-key (kbd "M-à") 'org-metadown  )
+  (local-set-key (kbd "M-ò") 'org-metaleft  )
+  (local-set-key (kbd "M-ù") 'org-metaright )
+
   ;; Xterm apparently does not generate such sequences.
-  (local-set-key (kbd "C-M-è") 'org-shiftmetaup    ) 
-  (local-set-key (kbd "C-M-à") 'org-shiftmetadown  ) 
-  (local-set-key (kbd "C-M-ò") 'org-shiftmetaleft  ) 
-  (local-set-key (kbd "C-M-ù") 'org-shiftmetaright ) 
- 
+  (local-set-key (kbd "C-M-è") 'org-shiftmetaup    )
+  (local-set-key (kbd "C-M-à") 'org-shiftmetadown  )
+  (local-set-key (kbd "C-M-ò") 'org-shiftmetaleft  )
+  (local-set-key (kbd "C-M-ù") 'org-shiftmetaright )
+
+  ;; Disable the S-Arrow keys, to support CUA-mode.
+  (local-unset-key (kbd "<S-up>")    )
+  (local-unset-key (kbd "<S-down>")  )
+  (local-unset-key (kbd "<S-left>")  )
+  (local-unset-key (kbd "<S-right>") )
+  (local-unset-key (kbd "<C-S-up>")    )
+  (local-unset-key (kbd "<C-S-down>")  )
+  (local-unset-key (kbd "<C-S-left>")  )
+  (local-unset-key (kbd "<C-S-right>") )
+
   )
 
 
@@ -130,7 +140,7 @@
 (add-hook 'orgtbl-mode-hook 'orgtbl-mode-setup-local-keys)
 
 ;; Link org-mode to remember-mode (which is not present on Emacs <22)
-(when (fboundp 'org-remember-insinuate) 
+(when (fboundp 'org-remember-insinuate)
   (org-remember-insinuate)
 )
 
@@ -151,14 +161,14 @@
 ;; Aquamacs miss some org-agenda keybindings!
 (defun org-agenda-mode-setup-local-keys()
   "Define/>Undefine of orgtbl-mode keys"
-  ;; Org Agenda Left/Right movements 
+  ;; Org Agenda Left/Right movements
   (define-key org-agenda-mode-map (kbd "<left>") 'org-agenda-earlier)
   (define-key org-agenda-mode-map (kbd "<right>") 'org-agenda-later)
 )
 (add-hook 'org-agenda-mode-hook 'org-agenda-mode-setup-local-keys)
 
 ;; Weather in Org-Agenda
-(when-running-MacOSX 
+(when-running-MacOSX
  (setq org-google-weather-icon-directory (concat MacUser-site-lisp "/google-weather-icons/"))
 )
 (require 'google-weather)
@@ -168,7 +178,7 @@
 
 (provide 'init-org-mode)
 ;; Local Variables:
-;; mode: emacs-lisp 
+;; mode: emacs-lisp
 ;; folded-file: t
-;; End: 
+;; End:
 
