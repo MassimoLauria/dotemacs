4f7d3f25c4e685e6ef18371e73b80abb56f1634d
Massimo Lauria <lauria.massimo@gmail.com>
2010-12-17 03:17:28 +0100

    C header templates

---
+++ templates/h-template.h
@@ -0,0 +1,27 @@
+/*
+   Copyright (C) 2010 by >>>NAME<<< <>>>EMAIL<<<>
+
+   Created   : ">>>TIME<<< >>>NAME<<<"
+   Time-stamp: " "
+
+   Description::
+
+   >>>POINT<<<
+
+
+*/
+
+
+/* Preamble */
+#ifndef  >>>HEADERNAME<<<
+#define  >>>HEADERNAME<<<
+
+#include <stdlib.h>
+
+
+
+/* Code */
+
+
+
+#endif /* >>>HEADERNAME<<< */
