94f7f637dcfd158f7c6ce078057392d2cb4ad7ed
Massimo Lauria <lauria.massimo@gmail.com>
2011-03-06 02:32:57 +0100

    Pymacs excluded

--- init-python.el
+++ init-python.el
@@ -49,18 +49,6 @@
             (python-auto-fill-comments-only)))
 
 
-;; Pymacs -----------------------------------------------------------------------------------------
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;; '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
-
-
 
 ;; Code checkers
 
