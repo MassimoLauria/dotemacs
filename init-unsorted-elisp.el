66e90c95653c8b78f387bea6e632d686fae334bd
Massimo Lauria <lauria.massimo@gmail.com>
2011-05-08 19:09:41 +0200

    Recent file opening looks better

--- init-unsorted-elisp.el
+++ init-unsorted-elisp.el
@@ -142,7 +142,7 @@
 (defun ido-recentf-open ()
   "Use `ido-completing-read' to \\[find-file] a recent file"
   (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+  (if (find-file (ido-completing-read "Find recent file: " (mapcar 'recentf-shorten-file-name recentf-list)))
       (message "Opening file...")
     (message "Aborting")))
 
