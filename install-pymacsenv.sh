af878b53d6fac662a8df0f4ad26d8fdfa848c3a0
Massimo Lauria <lauria.massimo@gmail.com>
2011-09-10 03:11:51 +0200

    Now the script to init python env is more compact

--- install-pymacsenv.sh
+++ install-pymacsenv.sh
@@ -3,22 +3,27 @@
 # Copyright (C) 2010, 2011 by Massimo Lauria <lauria.massimo@gmail.com>
 #
 # Created   : "2011-03-06, domenica 11:52 (CET) Massimo Lauria"
-# Time-stamp: "2011-03-07, lunedì 11:28 (CET) Massimo Lauria"
+# Time-stamp: "2011-09-10, Saturday 03:10 (CEST) Massimo Lauria"
 
 # Description::
 #
-# Install a python environment suitable for the last version of Pymacs and
-# Ropemacs. Everything in user space.
+# Install a python environment suitable for Pymacs and other several packages,
+# in order not to rely on the system environment.
 
 # Code::
 
-#URLs and VERSIONs
+# Python program and packages to be installed
+PYTHON=python2.7
+PKGS="pyflakes pylint pep8 pymacs rope ropemode ipython cython readline"
+
+# Pymacs version
 PYMACS=https://github.com/pinard/Pymacs/
 PYMACSVER=v0.24-beta2
 
 # Paths
 EMACSD=$HOME/.emacs.d/
 PYENV=$EMACSD/local-python
+PYENV=$HOME/prova/
 OLD_PWD=$PWD
 
 # Programs
@@ -26,114 +31,29 @@ cd $(dirname $0)
 VENV=$PWD/virtualenv.py
 
 
-# Utilities
-MKTEMP=mktemp
-MKDIR=mkdir
-RM=rm
-CP=cp
-MAKE=make
-GIT=git
-HG=hg
 
-
-# ------------------- Functions -----------------------------
-require_program()
-{
-    if [ $# -ne 1 ]; then echo "Wrong argument number."; exit 1; fi
-
-    echo -n "Checking for '$1'... "
-    which $1 2> /dev/null > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "FAIL."
-        exit_on_missing_program $1
-    else
-        echo "OK."
-    fi
-}
-
-exit_on_missing_program() {
-    echo ""
-    echo "Unfortunately program \"$1\" is not present, or it is not executable."
-    echo "Without this software, I can't finish the installation."
-    echo ""
-    echo "Bye bye."
-    exit $FILE_NOT_FOUND
-}
-
-
-find_mercurial_avoid_sagemath() {
-
-    which grep >/dev/null 2>/dev/null
-    if [ $? -ne 0 ]; then
-        echo "grep is not present. Can't test Mercurial to avoid sagemath installation."
-        return 2
-    fi
-
-    for hg in `which hg` "/usr/bin/hg" "/usr/local/bin/hg"; do
-        grep sage "$hg" >/dev/null
-        if [ $? -ne 0 -a -x $hg ]; then
-            echo "A useful Mercurial installation was found at $hg"
-            export HG=$hg
-            return 0
-        fi
-    done
-    return 2
-}
-
-
-find_mercurial_avoid_sagemath
-
-require_program $GIT
-require_program $HG
-require_program $CP
-require_program $RM
-require_program $MAKE
-require_program $MKDIR
-require_program "python"
-
-
-# Setup the temporary file location
-which  $MKTEMP 2> /dev/null 1> /dev/null
-if [ $? -eq 0 ]; then
-    TEMPDIR=`mktemp -d`
-else
-    TEMPDIR="/tmp/ropetemp"
-    $MKDIR -p $TEMPDIR
-fi
-
-echo "Setting up temp location $TEMPDIR"
-cd $TEMPDIR
-
-
-# Setup the environment
-$RM -fr $PYENV
-python $VENV $PYENV
+# Setup the basic environment
+echo "[1/3] Setup of basic environment."
+#rm -fr $PYENV
+#$PYTHON $VENV --clear --no-site-packages $PYENV
 . $PYENV/bin/activate
 
-# Download and Setup Rope
-$HG clone http://bitbucket.org/agr/rope
-$HG clone http://bitbucket.org/agr/ropemacs
-easy_install rope
-easy_install ropemacs
+# Install all packages (may require some ti)
+echo "[2/3] Packages installation (wait several minutes)."
+#$PYENV/bin/pip install $PKGS
 
-# Download and Setup Pymacs
-$GIT clone $PYMACS
+
+# Install
+echo "[3/3] Install Pymacs."
+git clone $PYMACS
 cd Pymacs
-$GIT co $PYMACSVER
-$MAKE
+git  co $PYMACSVER
+make
 easy_install .
-$RM -f $EMACSD/pymacs.el
-$CP pymacs.el $EMACSD
-
-# Cleanup the mess
-cd $OLD_PWD
-$RM -fr $TEMPDIR
-
-echo "*******************************************************************"
-echo "*  Add the following line to your init.el file                    *"
-echo "*                                                                 *"
-echo '* (setenv "PYMACS_PYTHON" "~/.emacs.d/local-python/bin/python")   *'
-echo "*******************************************************************"
+rm -f $EMACSD/pymacs.el
+cp pymacs.el $EMACSD
+cd ..
+rm -fr Pymacs
 
 exit 0
 
