44e799e110e4aec8308e087e2b65536123977505
Massimo Lauria <lauria.massimo@gmail.com>
2012-01-30 01:25:56 +0100

    Pymacs environment setup is now more robust

--- install-pymacsenv.sh
+++ install-pymacsenv.sh
@@ -1,10 +1,9 @@
-
 #!/bin/sh
 #
-# Copyright (C) 2010, 2011 by Massimo Lauria <lauria.massimo@gmail.com>
+# Copyright (C) 2010, 2011, 2012 by Massimo Lauria <lauria.massimo@gmail.com>
 #
 # Created   : "2011-03-06, domenica 11:52 (CET) Massimo Lauria"
-# Time-stamp: "2012-01-30, 00:41 (CET) Massimo Lauria"
+# Time-stamp: "2012-01-30, 01:14 (CET) Massimo Lauria"
 
 # Description::
 #
@@ -13,49 +12,76 @@
 
 # Code::
 
-# Python program and packages to be installed
-PYTHON=python
+
+# ---- Packages to be installed --------------------------------------
 PKGS="pyflakes pylint pep8 rope ropemode ipython cython readline"
 
-# Pymacs version
-PYMACS=https://github.com/pinard/Pymacs/
+PYMACS=https://github.com/pinard/Pymacs/   # Pymacs must be downloaded
 PYMACSVER=v0.24-beta2
 
-# Paths
+# ---- Installation paths --------------------------------------------
 EMACSD=$HOME/.emacs.d/
 PYENV=$EMACSD/local-python
+
+
+
+
+# ---- Other env variables -------------------------------------------
+CP=cp
+LN=ln
+RM=rm
+MKDIR=mkdir
+FILE_NOT_FOUND=127
+
+GIT=git
+PYTHON=python
+
+# Virtual env script
 OLD_PWD=$PWD
 
-# Programs
 cd $(dirname $0)
 VENV=$PWD/virtualenv.py
 
 
 
-# Setup the basic environment
+# -- 1 -- Setup the basic environment --------------------------------
 echo "[1/3] Setup of basic environment."
-rm -fr $PYENV
-$PYTHON $VENV --clear --no-site-packages $PYENV
-. $PYENV/bin/activate
+$RM -fr $PYENV
+$PYTHON $VENV --clear --no-site-packages $PYENV 2>/dev/null
 
-# Install all packages (may require some ti)
+if [ $? -ne 0 ]; then
+    echo "Some problems during setup. Aborting."
+    exit 1
+fi
+
+
+# -- 2 -- Install all packages ---------------------------------------
 echo "[2/3] Packages installation (wait several minutes)."
+. $PYENV/bin/activate
 $PYENV/bin/pip install $PKGS
 
 
-# Install
+# -- 3 -- Install pymacs ---------------------------------------------
 echo "[3/3] Install Pymacs."
-git clone $PYMACS
-cd Pymacs
-git  co $PYMACSVER
+
+
+$GIT clone $PYMACS
+cd $(basename "$PYMACS")
+$GIT co $PYMACSVER
+
 make
 easy_install .
-rm -f $EMACSD/pymacs.el
-cp pymacs.el $EMACSD
-cd ..
-rm -fr Pymacs
 
+$RM -f $EMACSD/pymacs.el
+$CP pymacs.el $EMACSD
+cd ..
+$RM -fr $(basename "$PYMACS")
+
+
+# Back to where we were...
+cd $OLD_PWD
 exit 0
+
 
 # Local Variables:
 # fill-column: 80
