bc8460b2af58ec62bf8e82f131a8526a9fd36a5a
Massimo Lauria <lauria.massimo@gmail.com>
2013-02-27 19:09:32 +0100

    Eval and Replace

--- init-functions.el
+++ init-functions.el
@@ -251,6 +251,17 @@ Emacs buffers are those whose name starts with *."
     )
   )
 
+;; Lisp specific defuns by Magnar Sveen
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 
 ;;;---- Recreate *scratch* buffer as soon as it's killed ------------------
 
