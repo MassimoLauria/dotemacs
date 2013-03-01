532cafb9ca4af4cdc269622f32816e38c58eef2e
Massimo Lauria <lauria.massimo@gmail.com>
2013-03-01 18:50:57 +0100

    Idle timer agenda

--- init-org-mode.el
+++ init-org-mode.el
@@ -280,6 +280,33 @@ buffer."
 
 
 
+(defun jump-to-org-agenda ()
+  (interactive)
+  (let ((buf (get-buffer "*Org Agenda*"))
+        wind)
+    (if buf
+        (if (setq wind (get-buffer-window buf))
+            (select-window wind)
+          (if (called-interactively-p)
+              (progn
+                (select-window (display-buffer buf t t))
+                (org-fit-window-to-buffer)
+                ;; (org-agenda-redo)
+                )
+            (with-selected-window (display-buffer buf)
+              (org-fit-window-to-buffer)
+              ;; (org-agenda-redo)
+              )))
+      (call-interactively 'org-agenda-list)))
+  ;;(let ((buf (get-buffer "*Calendar*")))
+  ;;  (unless (get-buffer-window buf)
+  ;;    (org-agenda-goto-calendar)))
+  )
+
+(run-with-idle-timer 300 t 'jump-to-org-agenda)
+
+
+
 ;; Patch org-bibtex-store-link to manage Capitalized Fields.
 (defun init-org-mode--patch-org-bibtex ()
   "The original `org-bibtex-store-link' does not accept capitalized fields."
