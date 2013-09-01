2417749d3f109caab4be00674f2376df472d3a6e
Massimo Lauria <lauria.massimo@gmail.com>
2013-09-01 15:58:26 +0200

    Some fixes in the configuration
    While working on the configuration,
    I temporarily disactivated some features and I forgot to
    activate them back.

--- init-local-preferences.el
+++ init-local-preferences.el
@@ -4,36 +4,22 @@
 ;;;-----------------------------------------------------------------
 
 
+;;;
+;;;  Default values
+;;;
+
+;; Display preferences
+(set-default 'prefs-activate-bigfont     nil) ; Big fonts for smallscreens
+(set-default 'prefs-activate-smallscreen nil) ; Monitor is small here
+(set-default 'prefs-activate-widescreen  t)   ; The screen aspect ration is wide
+
+
 ;; Load all Emacs specific personal preferences
 ;; and information
 (setq prefs-local-file-name "~/personal/conf/emacs.el")
 
 (when (file-readable-p prefs-local-file-name)
   (load-file prefs-local-file-name)
-)
-
-
-;;;
-;;;  Default values
-;;;
-
-;; Display preferences
-(setq prefs-activate-bigfont     nil) ; Big fonts for smallscreens
-(setq prefs-activate-smallscreen nil) ; Monitor is small here
-(setq prefs-activate-widescreen  nil) ; The screen aspect ration is wide
-
-
-;; Applications
-(setq prefs-activate-org-mode   t) ; The famous ORG-Mode! Yaiii!!
-
-
-;;;
-;;;  Host and env based preferences
-;;;
-(when (string= (getenv "LAPTOP") "yes")
-  (setq prefs-activate-smallscreen t)
-  (setq prefs-activate-bigfont     t)
-  (setq prefs-activate-widescreen  t)
 )
 
 (provide 'init-local-preferences)
