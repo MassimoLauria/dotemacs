325e9a84d32a611b4c11ea118ad5f2900018c538
Massimo Lauria <lauria.massimo@gmail.com>
2014-09-04 23:33:02 +0200

    raise-frame only of linux

--- init-server.el
+++ init-server.el
@@ -62,11 +62,12 @@
 ;; Katsumi Yamaoka posted this in 
 ;; http://article.gmane.org/gmane.emacs.devel:39702"
 
-(defadvice raise-frame (after make-it-work (&optional frame) activate)
-  (if (executable-find "wmctrl")
-      (call-process
-       "wmctrl" nil nil nil "-i" "-R"
-       (frame-parameter (or frame (selected-frame)) 'outer-window-id))))
+(when running-GNULinux
+  (defadvice raise-frame (after make-it-work (&optional frame) activate)
+    (if (executable-find "wmctrl")
+        (call-process
+         "wmctrl" nil nil nil "-i" "-R"
+         (frame-parameter (or frame (selected-frame)) 'outer-window-id)))))
 
 (provide 'init-server)
 ;;; init-clipboard.el ends here
