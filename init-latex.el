143756f8332e9c2a60606b94f7b3e649c70cf5a5
Massimo Lauria <lauria.massimo@gmail.com>
2014-11-06 16:00:36 +0100

    Auctex configuration upgraded for 11.88.

--- init-latex.el
+++ init-latex.el
@@ -3,6 +3,17 @@
 ;;;
 ;;;-----------------------------------------------------------------
 
+;;
+;; Many great features require AucTeX 11.88. 
+;;
+;; previous version of auctex would not support, e.g., forward/inverse
+;; search with Evince, do not have a proper support of compilation
+;; error management, ...
+;;
+;; Older versions of this file had workarounds. Now I decided to clean
+;; up the LaTeX since updating auctex with elpa is very easy.
+;;
+
 
 ;; Multifile support, completition, style, reverse search support, ...
 (setq TeX-auto-save t)
@@ -17,58 +28,21 @@
 
 (setq font-latex-fontify-sectioning 'color)
 
-;; Since version 11.86 of AUCTeX the inverse/forward search is
-;; implemented using source correlation.  Source correlation can be
-;; realized either with source-specials, as has always been; or using
-;; SyncTeX. The latter methods also works with PDF reader as Skin and
-;; Evince.  The method is *guessed wrongly* in Auctex, so I switch it
-;; according to TeX-PDF-mode.
-;;
-;; If AUCTeX implementation is below 11.86 source specials are used.
-(defun init-latex--forward-search-setup ()
-  "Setup the source special mode, depending on the AucTeX version loaded."
-  (interactive)
-  (if (not (boundp 'AUCTeX-version))
-      (error "AUCTeX not installed or not loaded"))
-  (let ((version (string-to-number AUCTeX-version)))
-    (cond
-     ((>= version 11.86)
-      (setq TeX-source-correlate-mode t)
-      (setq TeX-source-correlate-method 'source-specials) ; Default
-      (setq TeX-source-correlate-start-server t))
-     (t
-      (setq TeX-source-specials-mode t)
-      (setq TeX-source-specials-view-start-server t))
-     )))
+;; (defun init-latex--fix-viewer-invocation ()
+;;   "Fix viewer invocation by not asking for an annoying confirmation.
+;;       - avoid confirmation or editing of view command
+;;       - allows for function to be used as viewer (AucTeX >= 11.86)"
+;;   (interactive)
+;;   (when (not running-Aquamacs)
+;;     (let ((version (string-to-number AUCTeX-version)))
+;;       (cond
+;;        ((>= version 11.86)
+;;         (add-to-list 'TeX-command-list '("View" "%V" TeX-run-discard-or-function nil t :help "Run Viewer (no  confirmation)")))
+;;        (t
+;;         (add-to-list 'TeX-command-list '("View" "%V" TeX-run-discard nil t :help "Run Viewer (no confirmation)")))
+;;        ))))
 
-
-(defun init-latex--pdfmode-toggle ()
-  "Setup the proper environment for TeX PDF mode. Mainly the
-source-specials/synctex toggle."
-  (interactive)
-  (when (and (boundp 'AUCTeX-version)
-             (>= (string-to-number AUCTeX-version) 11.86))
-    (setq TeX-source-correlate-method-active
-          (if TeX-PDF-mode 'synctex 'source-specials))
-    ))
-
-(add-hook 'TeX-PDF-mode-hook 'init-latex--pdfmode-toggle)
-
-(defun init-latex--fix-viewer-invocation ()
-  "Fix viewer invocation by not asking for an annoying confirmation.
-      - avoid confirmation or editing of view command
-      - allows for function to be used as viewer (AucTeX >= 11.86)"
-  (interactive)
-  (when (not running-Aquamacs)
-    (let ((version (string-to-number AUCTeX-version)))
-      (cond
-       ((>= version 11.86)
-        (add-to-list 'TeX-command-list '("View" "%V" TeX-run-discard-or-function nil t :help "Run Viewer (no  confirmation)")))
-       (t
-        (add-to-list 'TeX-command-list '("View" "%V" TeX-run-discard nil t :help "Run Viewer (no confirmation)")))
-       ))))
-
-(eval-after-load "tex-site" '(init-latex--forward-search-setup))
+;; (eval-after-load "tex-site" '(init-latex--forward-search-setup))
 
 ;; These are the files that are produced by LaTeX processes.  It is annoying
 ;; that they show up while I'm trying to open a proper TeX file (or any other
@@ -86,6 +60,7 @@ source-specials/synctex toggle."
 (add-hook 'TeX-mode-hook 'flycheck-mode)
 (add-hook 'TeX-mode-hook 'turn-on-reftex)
 (add-hook 'TeX-mode-hook 'turn-on-flyspell)
+(add-hook 'TeX-mode-hook 'TeX-source-specials-mode)
 
 (when-available 'aquamacs-latex-viewer-support
       (add-hook 'TeX-mode-hook 'aquamacs-latex-viewer-support 'append)) ;; load reftex first
@@ -100,7 +75,6 @@ source-specials/synctex toggle."
                        (make-local-variable 'compilation-exit-message-function)
                        (setq compilation-exit-message-function 'nil)
                        (add-to-list 'LaTeX-verbatim-environments "comment")
-                       (message "call viewer fix.")
                        (init-latex--fix-viewer-invocation)
                        )))
 
@@ -109,8 +83,8 @@ source-specials/synctex toggle."
 (add-hook 'TeX-mode-hook
           '(lambda ()
              (define-key TeX-mode-map (kbd "<f9>")  'init-latex--make)
-             (define-key TeX-mode-map (kbd "<f10>") 'TeX-view)
-             (define-key TeX-mode-map (kbd "<f11>") 'TeX-pin-region)
+             (define-key TeX-mode-map (kbd "<f10>") 'TeX-pin-region)
+             (define-key TeX-mode-map (kbd "<f11>") 'TeX-previous-error)
              (define-key TeX-mode-map (kbd "<f12>") 'TeX-next-error)
              (define-key TeX-mode-map (kbd "M-<f11>") 'previous-error)
              (define-key TeX-mode-map (kbd "M-<f12>") 'next-error)))
@@ -201,120 +175,6 @@ started."
                  (fboundp 'sp-up-sexp))
                 (define-key massimo-keyboard-mode-map (kbd "M-p") 'sp-up-sexp))))
 
-
-;; Setup DBUS communication between Evince and AUCTeX using SyncTeX
-;; Forward/inverse search with evince using D-bus.
-;; Forward search does not work with C-c C-v
-(if (and (require 'dbus "dbus" t) (executable-find "evince"))
-    (progn
-
-      (defvar auctex-evince-inverse-open-file t
-        "Inverse search will open the target file if it is not.")
-
-      (require 'url-util nil t)
-
-      ;;; Inverse search setup (C-leftclick in Evince --> Open in Emacs).
-      ;; Adapted from: http://www.mail-archive.com/auctex@gnu.org/msg04175.html
-      ;; Changed for Gnome3 according to
-      ;; http://ubuntuforums.org/showthread.php?p=11010827#post11010827
-      ;;
-      ;; Between Evince 2.32 and 3.2 Evince started to give url
-      ;; instead of filenames.
-
-      ;; `dbus-un-urlify' and `dbus-urlify-escape-only' should be improved to handle
-      ;; all special characters, not only spaces.
-      ;; The fix for spaces is based on the first comment on
-      ;; http://emacswiki.org/emacs/AUCTeX#toc20
-
-      (defun dbus-un-urlify (fname-or-url)
-        "A trivial function that replaces a prefix of file:/// with just /."
-        (if (string= (substring fname-or-url 0 8) "file:///")
-            (url-unhex-string (substring fname-or-url 7))
-          fname-or-url))
-
-      (defun dbus-urlify-escape-only (path)
-        "Handle special characters for urlify. Charachter '/' is added temporarily to the non escaped characters, so that `url-hexify-string' won't remove path separator."
-        (let ((url-unreserved-chars (cons ?/ url-unreserved-chars )))
-          (url-hexify-string path)))
-
-      (defun dbus-urlify (absolute-path)
-        "Transform /absolute/path to file:///absolute/path for Gnome with very limited support for special characters"
-        (if (string= (substring absolute-path 0 1) "/")
-            (concat "file://" (dbus-urlify-escape-only absolute-path))
-          absolute-path))
-
-      (defun auctex-evince-inverse-sync (url linecol &rest timestamp)
-        (message "Try to open %s is not opened..." url)
-        (let (
-          (buf (get-buffer
-                (file-name-nondirectory
-                  (replace-regexp-in-string "%20" " " (dbus-un-urlify url)))))
-          (line (car linecol))
-          (col (cadr linecol)))
-          (if (and (null buf) auctex-evince-inverse-open-file)
-                  (find-file-existing (dbus-un-urlify url)))
-          (if (null buf)
-              (message "Sorry, cannot open %s..." url)
-        (switch-to-buffer buf)
-        (goto-line (car linecol))
-        (unless (= col -1)
-          (move-to-column col)))))
-
-      ;;; Forward search (View in Emacs --> Open in Evince ).
-      ;; Adapted from http://dud.inf.tu-dresden.de/~ben/evince_synctex.tar.gz
-      ;; Changed for Gnome3 according to
-      ;; http://ubuntuforums.org/showthread.php?p=11010827#post11010827
-      (defun auctex-evince-forward-sync (pdffile texfile line)
-        (let ((dbus-name
-           (dbus-call-method :session
-                     "org.gnome.evince.Daemon"  ; service
-                     "/org/gnome/evince/Daemon" ; path
-                     "org.gnome.evince.Daemon"  ; interface
-                     "FindDocument"
-                     (dbus-urlify pdffile)
-                     t     ; Open a new window if the file is not opened.
-                     ))
-              (time (current-time)))
-          (dbus-call-method :session
-                dbus-name
-                "/org/gnome/evince/Window/0"
-                "org.gnome.evince.Window"
-                "SyncView"
-                texfile
-                (list :struct :int32 line :int32 1)
-                (+ (* (car time) (expt 2 16)) (cadr time)))))
-
-      (defun auctex-evince-view ()
-        (interactive)
-        (let ((pdf (file-truename (concat default-directory
-                          (TeX-master-file (TeX-output-extension)))))
-          (tex (buffer-file-name))
-          (line (line-number-at-pos)))
-          (auctex-evince-forward-sync pdf tex line)))
-
-
-      (defun init-latex--evince--register ()
-        "Evince is connected to Emacs Auctex using DBUS"
-        (setq TeX-evince-dbus-registered t)
-        (condition-case nil
-            (dbus-register-signal
-             :session nil "/org/gnome/evince/Window/0"
-             "org.gnome.evince.Window" "SyncSource"
-             'auctex-evince-inverse-sync)
-          (error (setq TeX-evince-dbus-registered nil)))
-
-        (when (and TeX-evince-dbus-registered
-                   (boundp 'TeX-source-correlate-method))
-          ;; View program and selection rule
-          ;; for this to work `TeX-run-discard-or-function' must be in the "View" command.
-          (add-to-list 'TeX-view-program-list '("EvinceDbus" auctex-evince-view))
-          (add-to-list 'TeX-view-program-selection '(output-pdf "EvinceDbus"))
-          ))
-
-      ;; Register inverse and forward search
-      (eval-after-load "tex" '(init-latex--evince--register))
-
-      )) ;; D-Bus + Evince + SyncTeX
 
 
 ;; For MacOSX I tend to use Skim viewer for PDFs. Skim is already set
@@ -533,117 +393,6 @@ Do that only if the file do not exists already."
     ))
 
 (add-hook 'LaTeX-mode-hook 'choose-initial-latex-template)
-
-
-(defun bugfix-TeX-parse-error (old)
-  "Goto next error.  Pop to OLD buffer if no more errors are found.
-
- --- Parse error bug fix for Debian and Ubuntu and other TeX Live
-
- Original `TeX-parse-error' gets confused by TeX version number
- containing the string (TeX Live 20XX Debian), the result being
- creating stray files and buffers. 
-
- Based on http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=628790"
-  (let ((regexp
-	 (concat
-	  ;; TeX error
-	  "^\\(!\\|\\(.*?\\):[0-9]+:\\) \\|"
-	  ;; New file
-	  "(\\(\"[^\"]*?\"\\|/*\
-\\(?:\\.+[^()\r\n{} \\/]*\\|[^()\r\n{} .\\/]+\
-\\(?: [^()\r\n{} .\\/]+\\)*\\(?:\\.[-0-9a-zA-Z_.]*\\)?\\)\
-\\(?:[\\/]+\\(?:\\.+[^()\r\n{} \\/]*\\|[^()\r\n{} .\\/]+\
-\\(?: [^()\r\n{} .\\/]+\\)*\\(?:\\.[-0-9a-zA-Z_.]*\\)?\\)?\\)*\\)\
-)*\\(?: \\|\r?$\\)\\|"
-	  ;; End of file
-	  "\\()\\))*\\|"
-	  ;; Hook to change line numbers
-	  " !\\(?:offset(\\([---0-9]+\\))\\|"
-	  ;; Hook to change file name
-	  "name(\\([^)]+\\))\\)\\|"
-	  ;; LaTeX bad box
-	  "^\\(\\(?:Overfull\\|Underfull\\|Tight\\|Loose\\)\
- \\\\.*?[0-9]+--[0-9]+\\)\\|"
-	  ;; LaTeX warning
-	  "^\\(LaTeX [A-Za-z]*\\|Package [A-Za-z]+ \\)Warning:.*")))
-    (while
-	(cond
-	 ((null
-	   (re-search-forward regexp nil t))
-	  ;; No more errors.
-	  (message "No more errors.")
-	  (beep)
-	  (TeX-pop-to-buffer old)
-	  nil)
-	 ;; TeX error
-	 ((match-beginning 1)
-	  (when (match-beginning 2)
-	    (unless TeX-error-file
-	      (push nil TeX-error-file)
-	      (push nil TeX-error-offset))
-	    (unless (car TeX-error-offset)
-	      (rplaca TeX-error-file (TeX-match-buffer 2))))
-	  (if (looking-at "Preview ")
-	      t
-	    (TeX-error)
-	    nil))
-	 ;; LaTeX bad box
-	 ((match-beginning 7)
-	  (if TeX-debug-bad-boxes
-	      (progn
-		(TeX-warning (TeX-match-buffer 7))
-		nil)
-	    (re-search-forward "\r?\n\
-\\(?:.\\{79\\}\r?\n\
-\\)*.*\r?$")
-	    t))
-	 ;; LaTeX warning
-	 ((match-beginning 8)
-	  (if TeX-debug-warnings
-	      (progn
-		(TeX-warning (TeX-match-buffer 8))
-		nil)
-	    t))
-
-	 ;; New file -- Push on stack
-	 ((match-beginning 3)
-	  (let ((file (TeX-match-buffer 3))
-		(end (match-end 3)))
-	    ;; Strip quotation marks and remove newlines if necessary
-	    (when (or (eq (string-to-char file) ?\")
-		      (string-match "\n" file))
-	      (setq file
-		    (mapconcat 'identity (split-string file "[\"\n]+") "")))
-        (when (not (string-match "^TeX\\ Live\\ 20[0-9][0-9]\\/?\\ ?\\(\ Debian\\|\ Ubuntu\\)?$" file))
-	    (push file TeX-error-file)
-	    (push nil TeX-error-offset)
-	    (goto-char end)))
-	  t)
-	 
-	 ;; End of file -- Pop from stack
-	 ((match-beginning 4)
-	  (when (> (length TeX-error-file) 1)
-	    (pop TeX-error-file)
-	    (pop TeX-error-offset))
-	  (goto-char (match-end 4))
-	  t)
-	 
-	 ;; Hook to change line numbers
-	 ((match-beginning 5)
-	  (setq TeX-error-offset
-		(list (string-to-number (TeX-match-buffer 5))))
-	  t)
-	 
-	 ;; Hook to change file name
-	 ((match-beginning 6)
-	  (setq TeX-error-file
-		(list (TeX-match-buffer 6)))
-	  t)))))
-
-;; Use the version with bug fix
-(eval-after-load "tex-buf" '(defalias 'TeX-parse-error 'bugfix-TeX-parse-error))
-(eval-after-load "tex" '(require 'tex-buf))
 
 
 ;; Smart parenthesis
