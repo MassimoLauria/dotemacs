4dde2ba46df6c7e0b7cd4a60719fc19462f75397
Massimo Lauria <lauria.massimo@gmail.com>
2014-12-07 12:25:50 +0100

    Irony server installer is more clever now

--- install-irony-server.sh
+++ install-irony-server.sh
@@ -5,16 +5,21 @@
 # compiler.
 
 # Irony server source and destination path
-IRONYSERVER_SRC=$HOME/.emacs.d/elpa/irony-20141204.1531/server
+IRONYSERVER_SRC=`ls -d $HOME/.emacs.d/elpa/irony-*/|tail -1`server
 IRONYSERVER_INSTALL_PATH=$HOME/.emacs.d/irony/
 
+# build in a safe place
+TEMPDIR=`mktemp -d -t irony`
+pushd $TEMPDIR >/dev/null
 
 # We need the libclang location...
 LIBCLANG_LIBRARY=`xcode-select --print-path`/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib
+echo "LIBCLANG: library found at $LIBCLANG_LIBRARY"
 
 # and the headers that (AAARGH!!) Xcode does not ship. The headers are
 # relatively stable, so we can download them.
-svn export http://llvm.org/svn/llvm-project/cfe/trunk/include/clang-c/
+svn export http://llvm.org/svn/llvm-project/cfe/trunk/include/clang-c/ >/dev/null 
+echo "LIBCLANG: library headers downloaded to $PWD/clang-c"
 
 cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
       -DCMAKE_INSTALL_PREFIX=$IRONYSERVER_INSTALL_PATH \
@@ -23,3 +28,7 @@ cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
       $IRONYSERVER_SRC \
     && \
 cmake --build . --use-stderr --config Release --target install
+
+# cleanup
+popd >/dev/null 
+rm -fr $TEMPDIR
