b9f9f9d0b023a7e4e0c9c39f61706a47e2e63429
Massimo Lauria <lauria.massimo@gmail.com>
2015-03-10 10:58:52 +0100

    Sensible alias to run mathematica in Emacs

--- init-mathematica.el
+++ init-mathematica.el
@@ -11,10 +11,12 @@
        (executable-find "/Applications/Mathematica.app/Contents/MacOS/MathKernel")
        (executable-find "/pkg/mathematica/10.0.2/os/bin/MathKernel")))
 
+
 (use-package wolfram-mode
   :commands (run-wolfram wolfram-mode)
   :mode ("\\.m$" . wolfram-mode))
 
+(defalias 'run-mathematica 'run-wolfram)
 
 (provide 'init-mathematica)
 ;; Local Variables:
