f9eda8a9b29ee7956dcff6e8db0129087922b22f
Massimo Lauria <lauria.massimo@gmail.com>
2015-08-30 23:50:32 +0200

    Upgrade the template for python projects.

--- templates/python-project/setup.py
+++ templates/python-project/setup.py
@@ -3,7 +3,7 @@
 Setup script for the __PROJECT-NAME__ package
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='__PROJECT-NAME__',
       version='0.0.0.1',
@@ -11,7 +11,7 @@ setup(name='__PROJECT-NAME__',
       author='__USER-NAME__',
       author_email='__USER-MAIL-ADDRESS__',
       # url='',
-      packages=['__PROJECT-NAME__'],
+      packages = find_packages(".", exclude=["*.tests"]),
       license='__LICENSE-FILE-NAME__',
       install_requires=[],
       # make some tests
