a1dc29b8f7baa89d5ba19a8ba100303e3baa3c10
Massimo Lauria <lauria.massimo@gmail.com>
2016-06-15 16:09:12 +0200

    Remove useless preferences.

--- init-local-preferences.el
+++ init-local-preferences.el
@@ -8,11 +8,6 @@
 ;;;  Default values
 ;;;
 
-;; Display preferences
-(set-default 'prefs-activate-bigfont     nil) ; Big fonts for smallscreens
-(set-default 'prefs-activate-smallscreen nil) ; Monitor is small here
-(set-default 'prefs-activate-widescreen  t)   ; The screen aspect ration is wide
-
 
 ;; Load all Emacs specific personal preferences
 ;; and information
