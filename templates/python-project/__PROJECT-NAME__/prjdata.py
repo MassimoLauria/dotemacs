9e549894fd5985750770643f22858a67e0e920e0
Massimo Lauria <lauria.massimo@gmail.com>
2017-09-12 15:29:36 +0200

    Small improvements in the default python project

---
+++ templates/python-project/__PROJECT-NAME__/prjdata.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__project_name__        = '__PROJECT-NAME__'
+__project_description__ = '__DESCRIPTION__'
+
+__author__              = '__USER-NAME__'
+__author_email__        = '__USER-MAIL-ADDRESS__'
+__aurhor_string__       = __author__ + ' <' + __author_email__ + '>'
+
+__version_short__       = '0.0.0.1'
+__version_long__        = '0.0.0.1'
+__version__             = __version_long__
+
+__copyright__           = '2017 '+ __author__ + ' <' + __author_email__ + '>'
+__license__             = 'GPL-3'
+__url__                 = ''
+__repo_url__            = ''
--- templates/python-project/setup.py
+++ templates/python-project/setup.py
@@ -5,16 +5,23 @@ Setup script for the __PROJECT-NAME__ package
 
 from setuptools import setup, find_packages
 
-setup(name='__PROJECT-NAME__',
-      version='0.0.0.1',
-      description='__DESCRIPTION__',
-      author='__USER-NAME__',
-      author_email='__USER-MAIL-ADDRESS__',
-      # url='',
-      packages = find_packages(".", exclude=["*.tests"]),
-      license='__LICENSE-FILE-NAME__',
-      install_requires=[],
-      # make some tests
-      test_suite='nose.collector',
-      tests_require=['nose']
+import __PROJECT-NAME__.prjdata as p
+
+setup(
+    name         = p.__project_name__,
+    version      = p.__version__,
+    description  = p.__project_description__,
+    author       = p.__author__,
+    author_email = p.__author_email__,
+    url          = p.__url__,
+    license      = p.__license__,
+    packages     = find_packages(".", exclude=["tests"]),
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    install_requires=[],
+    # make some tests
+    test_suite='nose.collector',
+    tests_require=['nose']
 )
