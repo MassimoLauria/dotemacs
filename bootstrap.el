a5ac313adf209e69d37631fe485838d3f66e4ca8
Massimo Lauria <lauria.massimo@gmail.com>
2021-02-17 17:34:36 +0100

    Cmake by bootstrap

--- bootstrap.el
+++ bootstrap.el
@@ -24,6 +24,7 @@
                            async 
                            beacon 
                            bind-key 
+                           cmake-mode
                            company 
                            company-anaconda 
                            company-irony 
