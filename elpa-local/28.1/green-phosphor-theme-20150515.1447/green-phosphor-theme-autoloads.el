f0b6d716b911f6db3ac03b51bbee51d4e3b49b1a
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-01-13 15:41:42 +0100

    Green phosphor theme

---
+++ elpa-local/28.1/green-phosphor-theme-20150515.1447/green-phosphor-theme-autoloads.el
@@ -0,0 +1,23 @@
+;;; green-phosphor-theme-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "green-phosphor-theme" "green-phosphor-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from green-phosphor-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; green-phosphor-theme-autoloads.el ends here
---
+++ elpa-local/28.1/green-phosphor-theme-20150515.1447/green-phosphor-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from green-phosphor-theme.el  -*- no-byte-compile: t -*-
+(define-package "green-phosphor-theme" "20150515.1447" "A light color theme with muted, autumnal colors." 'nil :commit "5549781559ff5daa85c1d6c635c94524c1c5f644" :authors '(("Adam Alpern" . "adam.alpern@gmail.com")) :maintainer '("Adam Alpern" . "adam.alpern@gmail.com") :keywords '("color" "theme") :url "http://github.com/aalpern/emacs-color-theme-green-phosphor")
