4ad8c5371608b493a176f603c4cb0c57b0ff2677
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-03-10 03:10:38 +0100

    New completion framework

---
+++ elpa-local/29.1/espotify-20220121.2057/espotify-pkg.el
@@ -0,0 +1,14 @@
+(define-package "espotify" "20220121.2057" "Spotify access library"
+  '((emacs "26.1"))
+  :commit "ea6d6021e5acc550560325db2f09198839ee702f" :authors
+  '(("Jose A Ortega Ruiz" . "jao@gnu.org"))
+  :maintainers
+  '(("Jose A Ortega Ruiz"))
+  :maintainer
+  '("Jose A Ortega Ruiz")
+  :keywords
+  '("multimedia")
+  :url "https://codeberg.org/jao/espotify")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
