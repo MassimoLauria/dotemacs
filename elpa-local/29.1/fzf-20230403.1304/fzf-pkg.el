599a603ccbf336080c09a08003ffad97345916b7
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-05-20 16:31:03 +0200

    Fzf package

---
+++ elpa-local/29.1/fzf-20230403.1304/fzf-pkg.el
@@ -0,0 +1,14 @@
+(define-package "fzf" "20230403.1304" "A front-end for fzf."
+  '((emacs "24.4"))
+  :commit "3a55b983921c620fb5a2cc811f42aa4daaad8266" :authors
+  '(("Bailey Ling"))
+  :maintainers
+  '(("Bailey Ling"))
+  :maintainer
+  '("Bailey Ling")
+  :keywords
+  '("fzf" "fuzzy" "search")
+  :url "https://github.com/bling/fzf.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
