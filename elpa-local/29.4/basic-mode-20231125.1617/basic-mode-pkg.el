77b8b8561069d225f1d12901cafc68882443b056
Massimo Lauria <massimo.lauria@uniroma1.it>
2025-08-22 11:15:00 +0200

    Basic mode

---
+++ elpa-local/29.4/basic-mode-20231125.1617/basic-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "basic-mode" "20231125.1617"
+  "Major mode for editing BASIC code."
+  '((seq   "2.20")
+    (emacs "25.1"))
+  :url "https://github.com/dykstrom/basic-mode"
+  :commit "1dc1a635d6d80668c8a583b974205e49ff0fc3ce"
+  :revdesc "1dc1a635d6d8"
+  :keywords '("basic" "languages"))
